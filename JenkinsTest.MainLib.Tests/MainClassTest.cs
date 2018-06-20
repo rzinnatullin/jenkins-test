adc2564c2c5676c2ea6170a6c7aaa16308778692
Rustem Zinnatullin <xirustam@gmail.com>
2018-06-20 07:02:46 -0700

    Projects are added

---
+++ JenkinsTest.MainLib.Tests/MainClassTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using JenkinsTest.MainLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace JenkinsTest.MainLib.Tests
+{
+    [TestClass]
+    public class MainClassTest
+    {
+        [TestMethod]
+        public void Test_Sum_PositiveValues_ReturnsPositiveSum()
+        {
+            // Arrange
+            int a = 10;
+            int b = 20;
+            const int expectedResult = 30;
+            var mainClass = new MainClass();
+
+            // Act
+            var result = mainClass.Sum(a, b);
+
+            // Assert
+            result.ShouldBe(expectedResult);
+        }
+
+        [TestMethod]
+        public void Test_Divide_DividesOnePositiveByOtherPositive_ReturnsPositive()
+        {
+            // Arrange
+            int a = 300;
+            int b = 15;
+            const int expectedResult = 20;
+            var mainClass = new MainClass();
+
+            // Act
+            var result = mainClass.Divide(a, b);
+
+            // Assert
+            result.ShouldBe(expectedResult);
+        }
+    }
+}
---
+++ JenkinsTest.MainLib/MainClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsTest.MainLib
+{
+    public class MainClass
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return a / b;
+        }
+    }
+}
