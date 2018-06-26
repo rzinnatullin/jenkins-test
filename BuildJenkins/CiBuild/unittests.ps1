502e5a1a13a11a7970255f3acb682010efee3809
Rustem Zinnatullin <xirustam@gmail.com>
2018-06-25 22:40:37 -0700

    Jenkins: use vstest

--- BuildJenkins/CiBuild/unittests.ps1
+++ BuildJenkins/CiBuild/unittests.ps1
@@ -19,15 +19,4 @@ Remove-Item -Path $testResultDir -Recurse -ErrorAction:SilentlyContinue
 New-Item -ItemType Directory -Force -Path $testResultDir > $null
 
 Write-Host 'Executing unit tests and collecting coverage'
-OpenCover.Console.exe `
-  -target:"vstest.console.exe" `
-  -targetargs:"/Logger:trx /Platform:x64 /InIsolation $assmsToRun" `
-  -output:"$testResultDir\OpenCover-coverage.xml" `
-  -filter:"-[*.Tests]* +[*]*" `
-  -register
-
-Write-Host 'Converting OpenCover coverage to Cobertura format'
-OpenCoverToCoberturaConverter.exe `
-  -input:"$testResultDir\OpenCover-coverage.xml" `
-  -output:"$testResultDir\Cobertura-coverage.xml" `
-  -sources:"$sourcesRoot"
+vstest.console.exe /Logger:trx /Platform:x64 /InIsolation /EnableCodeCoverage $assmsToRun
